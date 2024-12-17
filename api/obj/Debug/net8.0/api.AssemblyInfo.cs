10744bd5e3669c499f1616650b79376d4a970a8e
ducknh <anhducb2013@gmail.com>
2024-12-17 19:57:47 +0700

    commit save changes

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f5095e0a0225158e03567ab28d8d07b9394759")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4d269a8522e33f8bf9db93c7142d4dfd2f1a48c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
