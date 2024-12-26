faaefef6986c0693b12ff0efe9286fd7b7879376
kuandearu <khanhquan1102@gmail.com>
2024-12-26 23:24:42 +0700

    Admin CRUD

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -6,6 +6,10 @@ import KlassList from "./pages/Klass/KlassList";
 import AddKlass from "./pages/Klass/AddKlass";
 import UpdateKlass from "./pages/Klass/UpdateKlass";
 import RegisterPage from "./pages/Register/RegisterPage";
+import AdminList from "./pages/Admin/AdminList";
+import AdminDetail from "./pages/Admin/AdminDetail";
+import AddAdmin from "./pages/Admin/AddAdmin";
+import UpdateAdmin from "./pages/Admin/UpdateAdmin";
 
 
 function App() {
@@ -23,6 +27,10 @@ function App() {
             path="/admin/update-class/:klassId"
             element={<UpdateKlass />}
           />
+          <Route path="/admin/admin-list" element={<AdminList />} />
+          <Route path="/admin/add-admin" element={<AddAdmin />} />
+          <Route path="/admin/update-admin/:id" element={<UpdateAdmin />} />
+          <Route path="/admin/detail/:id" element={<AdminDetail />} />
         </Routes>
       </Router>
     </>
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476bb81f42aeacd9b0996fef16a4cd5d3ba6cf03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e510b6d588b9d8165daf8f8d147d9c0ba4eb41")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
